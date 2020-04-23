9919227ba441eb90cf3c745abcd290b47914e432
Ondra Vaculik <vaculik.ond@gmail.com>
2020-04-23 14:58:19 +0200

    Switched fasta prepr tools

--- fasta_prepr/all_ref_1_file_simple_headers.py
+++ fasta_prepr/all_ref_1_file_simple_headers.py
@@ -4,9 +4,9 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--reference', '-r', type=str,
+parser.add_argument('--reference', '-r', type=str, required=True,
     help='Relative or absolute path to the folder containing\
-         one or multiple reference fasta files', required=True)
+         one or multiple reference fasta files')
 parser.add_argument('--outfile', '-out', type=str, default='.',
     help='Specify absolute or relative path to output directory, default = .')
 args = parser.parse_args()
@@ -15,24 +15,22 @@ args = parser.parse_args()
 p = Path(args.outfile)
 ref_files = Path(args.reference)
 
+references = {}
+
 # creating dict with all references
-all_references = {}
 for reference in ref_files.glob('*.fa*'):
-    header = ''
-    sequence = ''
-    with open(reference) as f:
-        ref_path = Path(reference)
-        name = ref_path.stem
-        lines = f.readlines()
-        for i, line in enumerate(lines):
-            if i < len(lines)-1:
-                if line.startswith('>'):
-                    header += line.strip()
-                else:
-                    sequence += line.strip()
+    chr_name = reference.stem
+    chr_sequence = ''
+    with open(reference, 'r') as rf:
+        for line in rf.readlines():
+            if line.startswith('>'):
+                continue
+            elif line == '':
+                continue
             else:
-                sequence += line.strip()
-                all_references[header] = sequence
+                chr_sequence += line.strip()
+    references[chr_name] = chr_sequence
+    chr_sequence = ''
     
 # preparing files and folders
 d = date.today().isoformat()
@@ -41,6 +39,6 @@ output_folder.mkdir(parents=True, exist_ok=True)
 output_file_path = output_folder / f'all_references.fasta'
 
 with output_file_path.open("w", encoding ="utf-8") as of:
-    for header, seq in all_references.items():
-        of.write(f'{header}\n')
-        of.write(f'{seq}\n')
+    for chr_name, chr_sequence in references.items():
+        of.write(f'>{chr_name}\n')
+        of.write(f'{chr_sequence}\n')
