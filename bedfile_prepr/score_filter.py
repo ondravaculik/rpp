31ac5fde0e6b3323be37a8e8cf13256ec29a58b6
Ondra Vaculik <vaculik.ond@gmail.com>
2020-04-23 09:43:12 +0200

    Changed descriptions and default score_treshold

--- bedfile_prepr/score_filter.py
+++ bedfile_prepr/score_filter.py
@@ -4,12 +4,12 @@ from pathlib import Path
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--score_treshold', '-sc', type=float, default=5.5, 
-    help='Absolute path to the transcriptome bedfile')
+parser.add_argument('--score_treshold', '-sc', type=float, default=6., 
+    help='Define treshold for filtering out by score, default = 6.')
 parser.add_argument('--infile', '-in', type=str, required=True, 
-    help='Absolute path to folder with bedfiles containing binding sites positions')
+    help='Absolute path to the bedfiles with binding sites positions')
 parser.add_argument('--outfile', '-out', type=str, required=True, 
-    help='Specify absolute path to output folder')               
+    help='Specify absolute or relative path to output directory')               
 args = parser.parse_args()
 
 
@@ -24,5 +24,6 @@ for bed_f in bed_files.glob('*.bed'):
     output_file_name = protein + '_score_filtered.bed'
     output_file_path = output_dir / output_file_name
     
-    pybedtools.BedTool(position for position in bedfile if float(position.score) >= score_treshold)\
+    pybedtools.BedTool(position for position in bedfile\
+        if float(position.score) >= score_treshold)\
     .saveas(output_file_path)
