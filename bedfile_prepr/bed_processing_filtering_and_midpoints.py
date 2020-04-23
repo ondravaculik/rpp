8e7d8abb1e95fe3ad6cf44fb7549b1a1db6b264d
Ondra Vaculik <vaculik.ond@gmail.com>
2020-04-23 09:58:30 +0200

    now processing multiple files, new descriptions

--- bedfile_prepr/bed_processing_filtering_and_midpoints.py
+++ bedfile_prepr/bed_processing_filtering_and_midpoints.py
@@ -1,6 +1,5 @@
 import argparse
 import pybedtools
-import datetime
 from pybedtools.featurefuncs import midpoint
 from pathlib import Path
 
@@ -16,48 +15,63 @@ def by_chr_filtering(feature, chr_list, inverse):
         if feature.chrom not in chr_list:
             return feature
 
-# přidat ještě argument, že si user bude moct zvolit, zda chce i filtrovat podle score treshold
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--chromosomes', '-chr', type=str, required=True,
-    help='List of chromosomes delimited by comma, which will be filtered from the positions bedfile')
+    help='List of chromosomes delimited by comma, which will\
+         be filtered out from the bedfile')
 parser.add_argument('--inverse', '-i', type=bool, default=False, 
     help='Exclude the specified chromosomes? default = False')
-parser.add_argument('--input_file', '-in', type=str, required=True, 
-    help='Absolute path to bedfile with protein binding sites positions')
-parser.add_argument('--intermediate_output', '-int_out', type=str, default=None,
-    help='Specify folder to save intermediate_outputfile (filtered by chromosomes only)')
-parser.add_argument('--output_file', '-out', type=str, required=True, 
-    help='Specify folder to save the output bedfile')                
+parser.add_argument('--score_treshold', '-sc', type=float, default=0., 
+    help='Define treshold for filtering out by score, default = 0.')
+parser.add_argument('--infile', '-in', type=str, required=True, 
+    help='Absolute path to the bedfiles with binding sites positions')
+parser.add_argument('--intermediate_out', '-int_out', type=str, default=None,
+    help='Specify folder to save intermediate_outputfile\
+         (filtered by chromosomes only)')
+parser.add_argument('--outfile', '-out', type=str, default='.', 
+    help='Specify absolute or relative path to output directory, default = .')                
 args = parser.parse_args()
 
-chr_list = args.chromosomes.split(',')
 
 p = Path(args.output_file)
-dt = datetime.datetime.now().strftime("%d:%m:%Y-%H:%M")
+input_bed_files = Path(args.infile)
+output_dir = Path(args.outfile)
+output_dir.mkdir(parents=True, exist_ok=True)
 
 intermediate_out = None
+chr_list = args.chromosomes.split(',')
 
 if args.inverse == False:
-    if args.intermediate_output != None:
+    if args.intermediate_out != None:
         intermediate_out = 'inversed_filtered_only_false.bed'
     final_out = 'inversed_midpoints_false.bed'
 else:
-    if args.intermediate_output != None:
+    if args.intermediate_out != None:
         intermediate_out = 'inversed_filtered_only_true.bed'
     final_out = 'inversed_midpoints_true.bed'
 
-# preparing output files and folders
-output_folder = Path(f'{p}', 'results', 'preprocessed_bedfiles', f'{dt}')
-output_folder.mkdir(parents=True, exist_ok=True)
+score_treshold = args.score_treshold
+
 if intermediate_out != None:
-    intermediate_output_file_path = output_folder / f'{intermediate_out}'
+    intermediate_output_file_path = output_dir / f'{intermediate_out}'
 else:
     intermediate_output_file_path = None
-final_output_file_path = output_folder / f'{final_out}'
 
-pybedtools.BedTool(args.input_file)\
-    .filter(by_chr_filtering, chr_list, args.inverse)\
-    .saveas(intermediate_output_file_path)\
-    .each(midpoint)\
-    .saveas(final_output_file_path)
+if score_treshold == 0.:
+    final_output_file_path = output_dir / f'{final_out}'
+else:
+    final_output_file_path = output_dir / f'{final_out}_score_filtered'
+
+for bed_f in input_bed_files.glob('*.bed'):
+    bedfile = pybedtools.BedTool(bed_f)
+    protein = Path(bed_f).stem
+    output_file_name = protein + '_score_filtered.bed'
+    output_file_path = output_dir / output_file_name
+
+    pybedtools.BedTool(args.input_file)\
+        .filter(by_chr_filtering, chr_list, args.inverse)\
+        .saveas(intermediate_output_file_path)\
+        .each(position for position in bedfile if float(position.score) >= score_treshold)\
+        .each(midpoint)\
+        .saveas(final_output_file_path)
